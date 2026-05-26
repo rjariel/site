ef67b77d81ed8bc6f30c1d863955972cd6df6823
ReyJariel <Rey@globalonmedia.com>
2026-05-26 22:15:21 +0800

    updates the link for social media

--- js/app.js
+++ js/app.js
@@ -1510,8 +1510,10 @@ function Footer() {
   }, "Senior WordPress Engineer & AI Solutions Architect"), /*#__PURE__*/React.createElement("div", {
     className: "socials"
   }, /*#__PURE__*/React.createElement("a", {
-    href: "#",
-    "aria-label": "GitHub"
+    href: "https://github.com/rjariel",
+    "aria-label": "GitHub",
+    target: "_blank",
+    rel: "noopener noreferrer"
   }, /*#__PURE__*/React.createElement("svg", {
     width: "14",
     height: "14",
@@ -1520,8 +1522,10 @@ function Footer() {
   }, /*#__PURE__*/React.createElement("path", {
     d: "M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.39 7.86 10.91.58.1.79-.25.79-.55 0-.27-.01-1.16-.02-2.1-3.2.7-3.88-1.36-3.88-1.36-.52-1.33-1.28-1.68-1.28-1.68-1.05-.72.08-.7.08-.7 1.16.08 1.77 1.19 1.77 1.19 1.03 1.76 2.7 1.25 3.36.96.1-.75.4-1.25.73-1.54-2.55-.29-5.24-1.28-5.24-5.7 0-1.26.45-2.29 1.19-3.1-.12-.29-.51-1.47.11-3.06 0 0 .97-.31 3.18 1.18a11.05 11.05 0 015.79 0c2.2-1.49 3.17-1.18 3.17-1.18.63 1.59.23 2.77.12 3.06.74.81 1.18 1.84 1.18 3.1 0 4.43-2.69 5.41-5.25 5.69.41.36.78 1.06.78 2.14 0 1.54-.01 2.78-.01 3.16 0 .31.21.67.8.55C20.21 21.39 23.5 17.08 23.5 12 23.5 5.65 18.35.5 12 .5z"
   }))), /*#__PURE__*/React.createElement("a", {
-    href: "#",
-    "aria-label": "LinkedIn"
+    href: "https://www.linkedin.com/in/lester-jariel/",
+    "aria-label": "LinkedIn",
+    target: "_blank",
+    rel: "noopener noreferrer"
   }, /*#__PURE__*/React.createElement("svg", {
     width: "14",
     height: "14",
